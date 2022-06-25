c13f065771805273da20e2df3f547734cabea00d
Andrés Felipe Londoño Campos <78039311+ApidriuC@users.noreply.github.com>
2022-06-25 14:18:17 -0500

    Update Functions and Fix bugs

--- SignalRChat/ChatHub.cs
+++ SignalRChat/ChatHub.cs
@@ -10,72 +10,78 @@ namespace SignalRChat
     public class ChatHub : Hub
     {
         #region Data Members
-
+        //Lista de Usuarios Conectados
         static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        //Lista de Mensajes Enviados
         static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
 
         #endregion
 
         #region Methods
 
+        //Conexión de Usuarios
         public void Connect(string userName)
         {
             var id = Context.ConnectionId;
 
-
+            //Un contador que me asigna los ID's al usuario nuevo sin que se repitan
             if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
             {
+                //Añade al usuario al ID generado en la lista de Usuarios
                 ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
 
-                // send to caller
+                //Conecta ese nuevo usuario al Chat
                 Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
 
-                // send to all except caller client
+                //Envio a todos, excepto al usuario nuevo
                 Clients.AllExcept(id).onNewUserConnected(id, userName);
 
             }
 
         }
-
+        //Envio de Mensajes
         public void SendMessageToAll(string userName, string message)
         {
-            // store last 100 messages in cache
+            //Llama a la funcion que me almacena los mensajes en el Cache 
             AddMessageinCache(userName, message);
 
-            // Broad cast message
+            //Envia el mensaje a todos los usuarios del chat
             Clients.All.messageReceived(userName, message);
         }
 
-   
 
+        //Desconexión de Usuarios
         public override System.Threading.Tasks.Task OnDisconnected()
         {
+            //Me recorre los Usuarios y verifica la conexión de ellos
             var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            //Si la conexión es null, quiere decir que cerró la ventana
             if (item != null)
             {
+                //Me remueve el usuario
                 ConnectedUsers.Remove(item);
 
                 var id = Context.ConnectionId;
                 Clients.All.onUserDisconnected(id, item.UserName);
 
             }
-
+            //Retorno de la función
             return base.OnDisconnected();
         }
 
-     
+
         #endregion
 
         #region
-
+        //Funcion para almacenar mensajes en el cache
         private void AddMessageinCache(string userName, string message)
         {
+            //A la lista añadimos el mensaje que envió el usuario
             CurrentMessage.Add(new MessageDetail { UserName = userName, Message = message });
-
-            if (CurrentMessage.Count > 100)
+            //Al cargar la pagina el historial va a contener los ultimos mensajes, ese valor se declara acá
+            if (CurrentMessage.Count > 5)
                 CurrentMessage.RemoveAt(0);
         }
-
         #endregion
     }
 
